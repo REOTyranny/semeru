9ad36d933b3c8beda9491931561f8f4b2ea99a04
rpaulucci3 <rafaelpaulucci2016@gmail.com>
2018-09-20 09:33:12 -0400

    Changed build-tools to latest (28.0.2); created POJO User model

---
+++ app/src/main/java/com/reotyranny/semeru/Model/User.java
@@ -0,0 +1,13 @@
+package com.reotyranny.semeru.Model;
+
+/**
+ * Represents a User.
+ * @author rpaulucci3
+ */
+public class User {
+
+    private String name;
+    private String password;
+    private String email;
+
+}
