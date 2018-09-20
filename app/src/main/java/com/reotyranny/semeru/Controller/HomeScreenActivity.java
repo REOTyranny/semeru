87254cc02ffb8dc494e7508e826c7debd6c70891
REOTyranny <reotyranny@users.noreply.github.com>
2018-09-20 17:59:52 -0400

    Added home button functionality to home screen

---
+++ app/src/main/java/com/reotyranny/semeru/Controller/HomeScreenActivity.java
@@ -0,0 +1,42 @@
+package com.reotyranny.semeru.Controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.reotyranny.semeru.R;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button loginButton = (Button) findViewById(R.id.button_SignOut);
+        loginButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreenActivity.this, WelcomeScreenActivity.class));
+            }
+        });
+    }
+
+}
