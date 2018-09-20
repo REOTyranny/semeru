8f345a3cfd53c2211cd8b9b67d8bbec86014b345
REOTyranny <reotyranny@users.noreply.github.com>
2018-09-20 17:39:32 -0400

    Added button functionality

--- app/src/main/java/com/reotyranny/semeru/Controller/WelcomeScreenActivity.java
+++ app/src/main/java/com/reotyranny/semeru/Controller/WelcomeScreenActivity.java
@@ -1,11 +1,13 @@
 package com.reotyranny.semeru.Controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.reotyranny.semeru.R;
 
@@ -24,6 +26,23 @@ public class WelcomeScreenActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button registerButton = (Button) findViewById(R.id.button_SignUp);
+        registerButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeScreenActivity.this, SignUpScreenActivity.class));
+            }
+        });
+        Button loginButton = (Button) findViewById(R.id.button_Login);
+        loginButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeScreenActivity.this, LoginScreenActivity.class));
             }
         });
     }
