156d9a05221eaab483fcec2d666b4ecf69778c3d
rpaulucci3 <rafaelpaulucci2016@gmail.com>
2018-09-20 18:14:01 -0400

    Button functionality for Welcome Screen

--- app/src/main/java/com/reotyranny/semeru/Controller/HomeScreenActivity.java
+++ app/src/main/java/com/reotyranny/semeru/Controller/HomeScreenActivity.java
@@ -20,14 +20,14 @@ public class HomeScreenActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         Button loginButton = (Button) findViewById(R.id.button_SignOut);
         loginButton.setOnClickListener( new View.OnClickListener() {
