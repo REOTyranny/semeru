d325a38af5dff03ab5ce20d6018d271c2ec1b6b8
rpaulucci3 <rafaelpaulucci2016@gmail.com>
2018-09-20 17:17:17 -0400

    Fix getters and setters for User

--- app/src/main/java/com/reotyranny/semeru/Model/User.java
+++ app/src/main/java/com/reotyranny/semeru/Model/User.java
@@ -43,23 +43,12 @@ public class User {
         this.role = role;
     }
 
-    public User(String name, String password, String email){
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        }
-    public void setPass(String password){
-        this.password = password;
-    }
-    public void setUserName(String name){ // sets username
-    this.name = name;
-    }
-    public void setEmail(String email){
-        this.email = email;
-    }
-    public String getName(){return name;}
-    public String getPassword(){return password;}
-    public String getEmail(){return email;}
+//    public User(String name, String password, String email, int role){
+//        this.name = name;
+//        this.password = password;
+//        this.email = email;
+//        this.role = role;
+//        }
 
 }
 
