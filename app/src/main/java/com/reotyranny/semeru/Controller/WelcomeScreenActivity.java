d4d6fc9f6ddff535441079e566d608fdada3494a
REOTyranny <REOTyranny@users.noreply.github.com>
2018-09-21 13:52:52 -0400

    Deleted toolbar and restructured the welcome screen xml files.

--- app/src/main/java/com/reotyranny/semeru/Controller/WelcomeScreenActivity.java
+++ app/src/main/java/com/reotyranny/semeru/Controller/WelcomeScreenActivity.java
@@ -2,10 +2,7 @@ package com.reotyranny.semeru.Controller;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,10 +14,9 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        Button registerButton = (Button) findViewById(R.id.button_SignUp);
+
+        Button registerButton = findViewById(R.id.button_SignUp);
         registerButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,7 +24,7 @@ public class WelcomeScreenActivity extends AppCompatActivity {
             }
         });
 
-        Button loginButton = (Button) findViewById(R.id.button_Login);
+        Button loginButton = findViewById(R.id.button_Login);
         loginButton.setOnClickListener( new View.OnClickListener() {
 
             @Override
