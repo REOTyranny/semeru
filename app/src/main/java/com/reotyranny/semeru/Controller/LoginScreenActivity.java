715ff88c059e842e8e367c8f55b606f4ba284e6a
Matthew Van Schellebeeck <mvansch@gatech.edu>
2018-09-21 09:41:06 -0400

    working login check

--- app/src/main/java/com/reotyranny/semeru/Controller/LoginScreenActivity.java
+++ app/src/main/java/com/reotyranny/semeru/Controller/LoginScreenActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Button;
 import android.util.Log;
+import com.reotyranny.semeru.Model.User;
 
 import com.reotyranny.semeru.R;
 
@@ -22,29 +24,32 @@ public class LoginScreenActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
+        final User test_user = new User("test-user", "user", "pass", "test@gmail.com", 1 );
 
         Button registerButton = (Button) findViewById(R.id.button_Confirm);
         registerButton.setOnClickListener( new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                //startActivity(new Intent(LoginScreenActivity.this, HomeScreenActivity.class));
-                EditText editTextEmail = (EditText)findViewById(R.id.editText_Email);
+
+                // email (username)
+                EditText editTextEmail = findViewById(R.id.editText_Email);
                 String LoginEmail = editTextEmail.getText().toString();
 
-                EditText editTextPassword = (EditText)findViewById(R.id.editText_Password);
-                String LoginPassword = editTextEmail.getText().toString();
+                // password
+                EditText editTextPassword = findViewById(R.id.editText_Password);
+                String LoginPassword = editTextPassword.getText().toString();
 
-                Log.d("matt-test", "user: " + LoginEmail + " password: " + LoginPassword);
+                // incorrect login text
+                TextView badLoginText = findViewById(R.id.badLoginTextView);
+
+
+                if( test_user.getUsername().equals(LoginEmail) && test_user.getPassword().equals(LoginPassword) )
+                    startActivity(new Intent(LoginScreenActivity.this, HomeScreenActivity.class));
+                else
+                    badLoginText.setVisibility(View.VISIBLE);
+
+                //Log.d("matt-test", "user: " + LoginEmail + " password: " + LoginPassword);
 
             }
         });
