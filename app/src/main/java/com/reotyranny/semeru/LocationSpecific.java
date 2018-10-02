abd7aed7d82a15e464bde81f37935da82e981fd9
jordan <jvhirsch@gatech.edu>
2018-10-02 16:20:43 -0400

    Location list and location specifics pages created

---
+++ app/src/main/java/com/reotyranny/semeru/LocationSpecific.java
@@ -0,0 +1,13 @@
+package com.reotyranny.semeru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationSpecific extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_specific);
+    }
+}
