e7cd44566d6c826b4b1c5c772296707a335c958f
REOTyranny <reotyranny@users.noreply.github.com>
2018-10-04 17:59:22 -0400

    Tried out recycler view...

--- app/src/main/java/com/reotyranny/semeru/Controller/LocationAdapter.java
+++ app/src/main/java/com/reotyranny/semeru/Controller/LocationAdapter.java
@@ -19,49 +19,50 @@ public class LocationAdapter extends
         // Your holder should contain a member variable
         // for any view that will be set as you render a row
         public TextView nameTextView;
-        public Button messageButton;
+        public TextView addressTextView;
+        public Button specificButton;
 
-        // We also create a constructor that accepts the entire item row
-        // and does the view lookups to find each subview
         public ViewHolder(View itemView) {
             // Stores the itemView in a public final member variable that can be used
             // to access the context from any ViewHolder instance.
             super(itemView);
 
-            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
-            messageButton = (Button) itemView.findViewById(R.id.message_button);
+            nameTextView = (TextView) itemView.findViewById(R.id.text_Name);
+            addressTextView = (TextView) itemView.findViewById(R.id.text_Address);
+            specificButton = (Button) itemView.findViewById(R.id.button_Specfic);
         }
+    }
 
     // Usually involves inflating a layout from XML and returning the holder
     @Override
-    public LocationAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
         // Inflate the custom layout
-        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+        View locationView = inflater.inflate(R.layout.recycler_view_item, parent, false);
 
         // Return a new holder instance
-        RecyclerView.ViewHolder viewHolder = new RecyclerView.ViewHolder(contactView);
+        ViewHolder viewHolder = new ViewHolder(locationView);
         return viewHolder;
     }
     private List<Location> mLocation;
     // Pass in the contact array into the constructor
-    public ContactsAdapter(List<Location> location) {
+    public LocationAdapter(List<Location> location) {
         mLocation = location;
     }
     // Involves populating data into the item through holder
     @Override
-    public void onBindViewHolder(LocationAdapter.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
         // Get the data model based on position
-        /*Contact contact = mContacts.get(position);
+        Location location= mLocation.get(position);
 
         // Set item views based on your views and data model
-        TextView textView = viewHolder.nameTextView;
-        textView.setText(contact.getName());
-        Button button = viewHolder.messageButton;
-        button.setText(contact.isOnline() ? "Message" : "Offline");
-        button.setEnabled(contact.isOnline());*/
+        TextView nameView = viewHolder.nameTextView;
+        nameView.setText(location.getName());
+        TextView addressView = viewHolder.addressTextView;
+        addressView.setText(location.getAddress());
+        Button button = viewHolder.specificButton;
     }
 
     // Returns the total count of items in the list
