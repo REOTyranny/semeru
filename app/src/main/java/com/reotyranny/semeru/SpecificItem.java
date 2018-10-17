a361adedc2d11ec22dad0955035576f8d1585787
jordan <jvhirsch@gatech.edu>
2018-10-17 11:02:20 -0400

    Specific item screen

---
+++ app/src/main/java/com/reotyranny/semeru/SpecificItem.java
@@ -0,0 +1,13 @@
+package com.reotyranny.semeru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpecificItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specific_item);
+    }
+}
