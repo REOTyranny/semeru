ee8ba19374ac88f08033ff75e50808be69da5728
Matthew Van Schellebeeck <mvansch@gatech.edu>
2018-10-24 12:18:15 -0400

    fix category spinner on donation add

--- app/src/main/java/com/reotyranny/semeru/Controller/AddItemActivity.java
+++ app/src/main/java/com/reotyranny/semeru/Controller/AddItemActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -28,6 +30,15 @@ public class AddItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_item);
         FirebaseModel Model = FirebaseModel.getInstance();
 
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_Category);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                        R.array.category_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
         TextView locationText = findViewById(R.id.locationText);
         locationText.setText(Model.userLocation);
@@ -53,11 +64,13 @@ public class AddItemActivity extends AppCompatActivity {
                     String longDes = ((EditText)findViewById(R.id.editText_Full)).getText().toString();
                     String value = ((EditText)findViewById(R.id.editText_Value)).getText().toString();
                     String comments = ((EditText)findViewById(R.id.editText_Comments)).getText().toString();
+                    Spinner spinner = (Spinner) findViewById(R.id.spinner_Category);
+                    String category = spinner.getItemAtPosition(spinner.getSelectedItemPosition()).toString();
 
                     Location location = new Location(12, Model.userLocation, 1.0f, 2.0f, "test address",
                             "test-city", "test-state", "102", "Some type", "404 1234 123", "www.test.com");
 
-                    Donation donation = new Donation(location, shortDes, longDes, Float.parseFloat(value), "Kitchen", comments);
+                    Donation donation = new Donation(location, shortDes, longDes, Float.parseFloat(value), category, comments);
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         if (dataSnapshot.exists()) {
