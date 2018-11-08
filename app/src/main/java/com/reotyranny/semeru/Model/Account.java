81be552dbe36f483d8fc258304848ad9778a7c6d
jordan <jvhirsch@gatech.edu>
2018-11-08 17:09:42 -0500

    So many heckin Javadocs

--- app/src/main/java/com/reotyranny/semeru/Model/Account.java
+++ app/src/main/java/com/reotyranny/semeru/Model/Account.java
@@ -17,6 +17,13 @@ public class Account {
     public Account() {
     }
 
+    /**
+     * Creates a new Account object
+     *
+     * @param name the user's name
+     * @param email the user's email address
+     * @param acctType the account type
+     */
     public Account(String name, String email, AccountType acctType) {
         this.name = name;
         this.email = email;
@@ -24,6 +31,14 @@ public class Account {
         this.locked = false;
     }
 
+    /**
+     * Creates a new Account object
+     *
+     * @param name the user's name
+     * @param email the user's email address
+     * @param acctType the account type
+     * @param location the location where the user works
+     */
     public Account(String name, String email, AccountType acctType, String location) {
         this.name = name;
         this.email = email;
@@ -32,22 +47,42 @@ public class Account {
         this.locked = false;
     }
 
+    /**
+     * Gets the type of the Account object
+     * @return the Account type
+     */
     public AccountType getAcctType() {
         return acctType;
     }
 
+    /**
+     * Sets the type for the Account object
+     * @param acctType the account type
+     */
     public void setAcctType(AccountType acctType) {
         this.acctType = acctType;
     }
 
+    /**
+     * Gets the email of the Account object
+     * @return the Account's email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the email for the Account object
+     * @param email the given email address
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets the location where the user who's account this is works
+     * @return the
+     */
     public String getLocation() {
         return location;
     }
